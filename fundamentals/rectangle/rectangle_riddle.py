275fd4bb176f264582455a94b39afe863427e2c4
ilanna-freire <ilanna.freire@olxbr.com>
2025-11-04 20:02:58 -0300

    initial branch commit

---
+++ fundamentals/rectangle/rectangle_riddle.py
@@ -0,0 +1,12 @@
+
+# The Code
+length = 5
+width = 3
+
+# Calculate the area 
+area = length * width  # Fixed this line - changed + to *
+
+# Print the result
+print("The area of the rectangle is:", area)
+
+
