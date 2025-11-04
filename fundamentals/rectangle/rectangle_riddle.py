7b540e8d1e340d1feeb770797a884700f4152eff
ilanna-freire <ilanna.freire@olxbr.com>
2025-11-04 20:12:26 -0300

    rectangle task updated

--- fundamentals/rectangle/rectangle_riddle.py
+++ fundamentals/rectangle/rectangle_riddle.py
@@ -1,12 +1,12 @@
 
 # The Code
-length = 5
-width = 3
+length = 5          # Length of the rectangle
+width = 3           # Width of the rectangle
 
 # Calculate the area 
-area = length * width  # Fixed this line - changed + to *
+area = length * width  # Fixed this line - changed + to *   # Calculate the area of the rectangle
 
-# Print the result
+# Display the area
 print("The area of the rectangle is:", area)
 
 
